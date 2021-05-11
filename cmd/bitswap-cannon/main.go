f4d45f5bf3d3c799123dd7daf154874e10604661
Daniel Rammer <hamersaw@protonmail.com>
2021-05-11 08:19:03 -0500

    adding file to seeder(s) and cating from leecher(s)

--- cmd/bitswap-cannon/main.go
+++ cmd/bitswap-cannon/main.go
@@ -3,8 +3,13 @@ package main
 import (
     "flag"
     "fmt"
+    "io"
     "strings"
+    "sync"
+    "time"
     "os"
+
+    shell "github.com/ipfs/go-ipfs-api"
 )
 
 type flagList []string
@@ -20,6 +25,9 @@ func (i *flagList) String() string {
 
 func main() {
     // parse command line arguments
+    var bufferSize int
+    flag.IntVar(&bufferSize, "b", 4096, "size of reader buffer (bytes)")
+
     var filenames, leechers, seeders flagList
     flag.Var(&filenames, "f", "filename(s) to process")
     flag.Var(&leechers, "l", "host to leech file(s)")
@@ -34,15 +42,76 @@ func main() {
         os.Exit(1)
     }
 
-    fmt.Println("filenmaes:", filenames)
-    fmt.Println("leechers:", leechers)
-    fmt.Println("seeders:", seeders)
+    //fmt.Println("filenames:", filenames)
+    //fmt.Println("leechers:", leechers)
+    //fmt.Println("seeders:", seeders)
 
-    // TODO - load file(s) into seeders
+    // add file(s) to seeders
+    cids := make([]string, len(filenames))
+    for _, seeder := range seeders {
+        sh := shell.NewShell(seeder)
 
-    // TODO - add file(s) to bitswap ledger want on leechers
+        for fileIndex, filename := range filenames {
+            f, err := os.Open(filename)
+            if err != nil {
+                fmt.Println("failed to open file:", err)
+                continue
+            }
 
-    // TODO - wait to complete
+            cid, err := sh.Add(f)
+            if err != nil {
+                fmt.Println("failed to add file:", err)
+                continue
+            }
+
+            fmt.Println(filename + " " + cid)
+            cids[fileIndex] = cid
+        }
+    }
+
+    // cat file(s) (to nowhere) from each leecher
+    var wg sync.WaitGroup
+    for _, leecher := range leechers {
+        sh := shell.NewShell(leecher)
+
+        for _, cid := range cids {
+            wg.Add(1)
+            go func() {
+                defer wg.Done()
+                start := time.Now()
+
+                r, err := sh.Cat(cid)
+                if err != nil {
+                    fmt.Println("failed to read cid:", err)
+                    return
+                }
+
+                buf := make([]byte, bufferSize)
+                for {
+                    _, err := r.Read(buf)
+                    if err == io.EOF {
+                        break
+                    } else if err != nil {
+                        fmt.Println("failed to read bytes:", err)
+                        continue
+                    }
+                }
+
+                elapsed := time.Since(start)
+                fmt.Println("retrieved file in", elapsed)
+
+                err = r.Close()
+                if err != nil {
+                    fmt.Println("failed to close reader:", err)
+                }
+            }()
+        }
+    }
+
+    // wait for all retrievals to complete
+    wg.Wait()
 
     // TODO - output stats
+
+    // TODO - cleanup
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hamersaw/bitswap-cannon
 
 go 1.15
+
+require github.com/ipfs/go-ipfs-api v0.2.0
